5783045e8897a00d0cf3cd1b1fe5db564358ef53
NAME <mail@gmail.com>
2021-10-13 17:45:30 +0800

    update

--- narcNum/main.cpp
+++ narcNum/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	int in,cmp;
 	vector<int> ans;
-	cin >> in;
+	//cin >> in;
 	for (int i=100;i<1000;i++){
 		int tmp=i;
 		while (tmp){
@@ -16,7 +16,7 @@ int main(){
 		}
 	}
 	for (int i=0;i<ans.size();i++){
-		cout << ans[i];
+		cout << ans[i] << endl;
 	}
 	return 0;
 }
